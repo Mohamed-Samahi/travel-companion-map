805ceea97af2d6ed5037c8adf7613c771c648e20
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2023-02-06 18:27:11 +0200

    Finished

---
+++ src/components/api/index.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "X-RapidAPI-Key":
+            "7c99fbc014msh217fcde312db0b4p169670jsn2f8bcbe1eb21",
+          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
